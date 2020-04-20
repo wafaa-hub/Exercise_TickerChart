158b184a48b3d042a9163a1955e6e72d4392b8a3
wafaa-hub <wafaa@ticherchart.com>
2020-04-20 15:52:12 +0300

    some edits on marketWatchJsonParse function to get data

--- Exercise/app/src/main/java/com/example/exercise/MainActivity.java
+++ Exercise/app/src/main/java/com/example/exercise/MainActivity.java
@@ -8,16 +8,26 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.example.exercise.Controller.MySingletonVolley;
+import com.example.exercise.Model.Company;
+import com.example.exercise.Model.CompanyDetailsAdapter;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,7 +37,7 @@ import java.util.Iterator;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.marketwatch) RecyclerView marketWatchRecycler;
-    private ArrayList<CompanyDetails> companies = new ArrayList<>();
+    private ArrayList<Company> companies = new ArrayList<>();
     private CompanyDetailsAdapter companyDetailsAdapter;
     private RequestQueue requestQueue;
 
@@ -50,34 +60,41 @@ public class MainActivity extends AppCompatActivity {
     public void marketWatchJsonParse() {
 
         String url = "http://tickerchart.com/interview/marketwatch.json";
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
+                System.out.print(response);
 
-                        try {
-                            Iterator<String> keys = response.keys();
+                for(int i =0 ; i<  response.length() ; i++)
+                {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        Company company = new Company(jsonObject);
+                        companies.add(company);
+                        companyDetailsAdapter.notifyDataSetChanged();
 
-                            while(keys.hasNext()) {
-                                String key = keys.next();
-                                if (response.get(key) instanceof JSONObject) {
-                                    CompanyDetails companyDetailsObject = new CompanyDetails(response);
-                                    companies.add(companyDetailsObject);
-                                }
-                            }
-
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
-
+                    } catch (JSONException e) {
+                        throw  new RuntimeException(e);
                     }
-                }, new Response.ErrorListener() {
+                }
+
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                throw  new RuntimeException(error);
 
             }
         });
-         MySingletonVolley.getInstance(getApplicationContext()).addToRequestQue(request);
+
+        MySingletonVolley.getInstance(getApplicationContext()).addToRequestQue(request);
+
     }
 }
